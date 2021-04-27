e1d149f0ab7afc3192cca5dc5c12696a5b4b1241
Gabriel Hofer <leirbagabriel468@gmail.com>
2021-04-27 15:27:44 -0400

    idk

--- main.py
+++ main.py
@@ -2,26 +2,40 @@ import numpy as np
 
 
 class waterCA:
+  """
+    Initialize 2D grid 
 
-  def __init__(self,width,height):
-    self.__CA = np.zeros((width,height))
-    pass
+    @param rows - number of rows in 2D grid
+    @param cols - number of cols in 2D grid
+  """
+  def __init__(self, rows, cols):
+    self.__CA = np.zeros((rows, cols))
+
+  """
+    Get the value of the next state in the CA at (r,c)
+
+    @param r - row location of cell
+    @param c - column location of cell
+  """
+  def __next_cell(self,r,c): pass 
+
+  """
+    generate next state for every cell in the grid
     
-  def __elapse(self):
-    pass
+  """
+  def next_2d_grid(self): pass
+    for i in range(width):
+      for j in range(height):
 
   """
     water cells appear at the top of the 2d grid
     at a random x coordinate
   """
-  def __rain(self):
-    pass
+  def rain(self): pass
 
-  def __show(self):
-    pass
+  def show(self): pass
 
-  def __obstacles(self):
-    pass
+  def __obstacles(self): pass
 
 
 def main():
