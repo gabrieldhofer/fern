74e64ac327472f7f75ea6724e08436d31d42427e
Gabriel Hofer <leirbagabriel468@gmail.com>
2021-04-27 15:19:01 -0400

    ok

---
+++ main.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+class waterCA:
+
+  def __init__(self,width,height):
+    self.__CA = np.zeros((width,height))
+    pass
+    
+  def __elapse(self):
+    pass
+
+  """
+    water cells appear at the top of the 2d grid
+    at a random x coordinate
+  """
+  def __rain(self):
+    pass
+
+  def __show(self):
+    pass
+
+  def __obstacles(self):
+    pass
+
+
+def main():
+
+
+
+
+
+
+
+
+
+main()
+
+
+
