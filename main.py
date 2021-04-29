41ef24db7637f7c4493bc805ae5c7ea1315314f3
Gabriel Hofer <leirbagabriel468@gmail.com>
2021-04-28 21:49:43 -0400

    idk

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import matplotlib.pyplot as plt
 from random import randint
 import random
-          
-
 
 class Barnsley:
     def __transform(self,x,y):
         r = randint(1, 100)
         if r == 1:
             return (0), (0.16*y)
-        if r>= 16 and r<= 100:
-            return (0.85*x + 0.04*y), (-0.04*x + 0.85*y + 1.6)
         if r>= 9 and r<= 15:
             return (0.2*x - 0.26*y), (0.23*x + 0.22*y + 1.6)
         if r>= 2 and r<= 8:
             return (-0.15*x + 0.28*y), (0.26*x + 0.24*y + 0.44)
+        if r>= 16 and r<= 100:
+            return (0.85*x + 0.04*y), (-0.04*x + 0.85*y + 1.6)
 
     def __show(self):
         plt.scatter(self.__x, self.__y, s = 0.2, edgecolor ='green')
