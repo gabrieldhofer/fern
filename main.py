3db0d323ae2741c031e81cb6c9357235629eea60
Gabriel Hofer <leirbagabriel468@gmail.com>
2021-04-28 20:21:43 -0400

    idk

--- main.py
+++ main.py
@@ -5,45 +5,41 @@ import random
 
 
 class Barnsley:
-	def __call__(self, iterations):
-		x = []
-		y = []
-		x.append(0)
-		y.append(0)
-		current = 0
-		for i in range(1, iterations):
-			z = randint(1, 100)
-			  
-			# for the probability 0.01
-			if z == 1:
-			    x.append(0)
-			    y.append(0.16*(y[current]))
-			  
-			# for the probability 0.85    
-			if z>= 2 and z<= 86:
-			    x.append(0.85*(x[current]) + 0.04*(y[current]))
-			    y.append(-0.04*(x[current]) + 0.85*(y[current])+1.6)
-			  
-			# for the probability 0.07    
-			if z>= 87 and z<= 93:
-			    x.append(0.2*(x[current]) - 0.26*(y[current]))
-			    y.append(0.23*(x[current]) + 0.22*(y[current])+1.6)
-			  
-			# for the probability 0.07    
-			if z>= 94 and z<= 100:
-			    x.append(-0.15*(x[current]) + 0.28*(y[current]))
-			    y.append(0.26*(x[current]) + 0.24*(y[current])+0.44)
-			      
-			current += 1
-		plt.scatter(x, y, s = 0.2, edgecolor ='green')
+  def __transform(self,x,y):
+    r = randint(1, 100)
+    if r == 1:
+        return (0, 0.16*y)
+    if r>= 16 and r<= 100:
+        return (0.85*x + 0.04*y, -0.04*x + 0.85*y + 1.6)
+    if r>= 9 and r<= 15:
+        return (0.2*x - 0.26*y, 0.23*x + 0.22*y + 1.6)
+    if r>= 2 and r<= 8:
+        return (-0.15*x + 0.28*y, 0.26*x + 0.24*y + 0.44)
 
-		plt.axis('off')
-		plt.savefig('fern.png')
+  def __show(self):
+    plt.scatter(self.__x, self.__y, s = 0.2, edgecolor ='green')
 
-		plt.show()
+    plt.axis('off')
+    plt.savefig('fern.png')
+
+    plt.show()
+
+  def __call__(self, iterations):
+    self.__x = [0]
+    self.__y = [0]
+    for i in range(iterations):
+      x2, y2 = self.__transform(self.__x[-1], self.__y[-1])
+      self.__x.append(x2)
+      self.__y.append(y2)
+
+    print(self.__x)
+    print(self.__y)
 
 
 
+    self.__show()
 
 fern = Barnsley()
 fern(50000)
+
+
